085097fb592d5126a8f83e068a31b9a7001402e1
Peter Levinsky <pele@easj.dk>
2018-10-31 14:42:24 +0100

    Add project files.

---
+++ CoinRestService/Controllers/CoinBidsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using modelLibrary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoinRestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoinBidsController : ControllerBase
+    {
+
+        // data list of Bids
+        private static List<CoinBid> bids = new List<CoinBid>()
+        {
+            new CoinBid(1, "Gold DK 1640", 2500, "Mike"),
+            new CoinBid(2, "Gold NL 1764", 5000, "Anbo"),
+            new CoinBid(3, "Gold FR 1644", 0, "Auction"),
+            new CoinBid(4, "Gold FR 1644", 35000, "Hammer"),
+            new CoinBid(5, "Silver GR 333", 2500, "Mike")
+        };
+
+
+        // GET: api/CoinBids
+        [HttpGet]
+        [Route("")]
+        public IList<CoinBid> GetBids()
+        {
+            return bids;
+        }
+
+        // GET: api/CoinBids/5
+        [HttpGet]
+        [Route("{id}")]
+        public CoinBid GetOneBid(int id)
+        {
+            return bids.Find(c => c.Id == id);
+        }
+
+        // POST: api/CoinBids
+        [HttpPost]
+        [Route("")]
+        public bool AddCoinBid([FromBody]CoinBid newCoinBid)
+        {
+            CoinBid bid = GetOneBid(newCoinBid.Id);
+            if (bid == null) // no bid at this Id 
+            {
+                bids.Add(newCoinBid);
+                return true;
+            }
+
+            return false;
+        }
+
+       
+    }
+}
---
+++ modelLibrary/CoinBid.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace modelLibrary
+{
+    public class CoinBid
+    {
+        private int _id;
+        private String _genstand;
+        private int _bud;
+        private String _navn;
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Genstand
+        {
+            get => _genstand;
+            set => _genstand = value;
+        }
+
+        public int Bud
+        {
+            get => _bud;
+            set => _bud = value;
+        }
+
+        public string Navn
+        {
+            get => _navn;
+            set => _navn = value;
+        }
+
+        public CoinBid()
+        {
+        }
+
+        public CoinBid(int id, string genstand, int bud, string navn)
+        {
+            // todo lav id automatisk
+            _id = id;
+            _genstand = genstand;
+            _bud = bud;
+            _navn = navn;
+        }
+
+        public CoinBid(int id, string genstand)
+        {
+            _id = id;
+            _genstand = genstand;
+            _bud = 0;
+            _navn = "auktion";
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Genstand)}: {Genstand}, {nameof(Bud)}: {Bud}, {nameof(Navn)}: {Navn}";
+        }
+    }
+}
